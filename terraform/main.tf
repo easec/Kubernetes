a23e2c79149cf5b7e6f26d813232f4e71bc3a0c6
easec <mats.johannesson@easec.se>
2021-08-09 11:09:21 +0200

    Delete main.tf

--- terraform/main.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    storage_account_name     = "<YourAzureStorageAccountName>"
-    container_name           = "tfstate"
-    key                      = "codelab.microsoft.tfstate"
-    access_key               = "<YourStorageAccountAccessKey>"
-  }
-}
